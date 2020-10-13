1d9487acc63553ac503a84abb167ee79da17645b
Opilane TTHK <opilane@tthk.ee>
2020-10-13 09:56:29 +0300

    ChuckAPI

---
+++ ChuckAPI/Program.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+using Newtonsoft;
+
+namespace ChuckAPI
+{
+    class Program
+    {
+        private static string url = "https://api.sunrise-sunset.org/json?lat=59.4370&lng=24.7536&date=2020-10-13";
+        static void Main(string[] args)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+
+            using (var responsereader = new StreamReader(webStream))
+            {
+                var response = responsereader.ReadToEnd();
+                SunMode sunMode = JsonConvert.DeserializeObject<SunMode>(response);
+                Console.WriteLine($"Sunrise: {sunMode.Results.Sunrise}.");
+                Console.WriteLine($"Sunset: {sunMode.Results.Sunset}");
+                Console.WriteLine($"Status: {sunMode.Status}");
+            }
+        }
+    }
+}
